d09f7efc4d77c55ceca0dd69159ae79bb51e01d1
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2023-05-19 10:56:30 +0300

    docker-compose added

--- FireResistance.Web/Data/ApplicationDbContext.cs
+++ FireResistance.Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,31 @@
 ﻿using FireResistance.Core.Entities.SourceDataForCalculation.SourceDataBasic;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace FireResistance.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { }
+            : base(options)
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Database.EnsureCreated();
+        }
         public DbSet<ColumnFireIsWithFourSidesData> ColumnFireIsWithFourSidesData { get; set; }
         public DbSet<SlabWithRigidConnectionData> SlabWithRigidConnectionData { get; set; }
     }
