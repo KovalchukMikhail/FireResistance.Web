a3a4ba65e99ae1f03e7af8a8bdc112433e558baa
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2023-07-24 14:14:55 +0300

    The project has been moved to the src folder

---
+++ src/Tests/EquationsFromSpTests/EquationsFromSp468Tests.cs
@@ -0,0 +1,32 @@
+﻿using EquationsFromSp;
+using EquationsFromSp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationsFromSpTests
+{
+    public class EquationsFromSp468Tests
+    {
+        IEquationsFromSp468 equations;
+        public EquationsFromSp468Tests()
+        {
+            equations = new EquationsFromSp468();
+        }
+
+        [Fact]
+        public void GetRbWithGammaBt()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+    }
+}
